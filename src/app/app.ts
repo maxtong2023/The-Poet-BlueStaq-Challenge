ae11b3a721602a0b7b78a253d1c800146b410289
maxtong2023 <maxtong2027@u.northwestern.edu>
2025-10-29 21:22:55 -0500

    added the basic homescreen

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Poetdle');
+  images = [
+    { src: 'assets/shake.jpeg', delay: 0, rotation: -8, x: '20%', y: '15%' },
+    { src: 'assets/emily.jpeg', delay: 0.1, rotation: 12, x: '70%', y: '20%' },
+    { src: 'assets/poe.jpg', delay: 0.2, rotation: -15, x: '30%', y: '65%' },
+  ];
 }
